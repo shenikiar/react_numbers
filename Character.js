b7d73fa2ef7b6005da5d89b0190f1bc1c9c5a8cd
shenikiar <39305056+shenikiar@users.noreply.github.com>
2019-03-29 09:38:15 -0400

    Add files via upload

---
+++ Character.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+
+
+
+
+
+
+class Character extends Component {
+  constructor(props){
+        super(props);
+        this.state = {
+          json: {}
+        }
+    }
+
+    componentDidMount () {
+        const URL = 'https://aztro.sameerkumar.website/?sign=virgo&day=today';
+        fetch(URL, {
+            method: 'POST'
+        }).then(response => response.json())
+        .then(json => { this.setState({json}); });
+    }
+
+    render() {
+        return (
+            <>
+            <div>
+           <h2>Is This You?</h2>
+           <p>Hopefully what follows is a snippet of your personality on today. </p>
+        </div>
+          <div>
+              Current Date: {this.state.json.current_date} <br />
+              Compatibility: {this.state.json.compatibility} <br />
+              Lucky Number: {this.state.json.lucky_number} <br />
+              Lucky Time: {this.state.json.lucky_time} <br />
+              Color: {this.state.json.color} <br />
+              Date Range: {this.state.json.date_range} <br />
+              Mood: {this.state.json.mood} <br />
+              Description: {this.state.json.description} <br />
+          </div>
+            </>
+        );
+    }
+}
+
+        
+
+export default Character;
