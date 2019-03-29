d419d4edd39f915fa20187e81a2456b9ee91e796
shenikiar <39305056+shenikiar@users.noreply.github.com>
2019-03-29 09:39:33 -0400

    Add files via upload
    adding final api

---
+++ index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App.js"
+
+ReactDOM.render(
+  <App/>,document.getElementById('root')
+)
+
+{/*//const randomPic = new XMLHttpRequest();
+//const pic = 'https://random.dog/21060-3302-20249.jpg';
+//Http.open("GET", pic);
+//Http.send();
+
+//Http.onreadystatechange = () =>{
+  //  console.log(Http.response)
+//}   
+//or 
+//const Url='https://random.dog/21060-3302-20249.jpg';
+//fetch(Url)
+//.then(data=>{return data.json()})
+//.then(res=>{console.log(res)})*/}
+
+
+
